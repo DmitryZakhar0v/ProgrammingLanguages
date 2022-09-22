6f8462307ab66110702a96512d63bcb091b05052
DmitryZakhar0v <pcixtimessquare@gmail.com>
2022-09-22 20:40:21 +0300

    Задание 1.2

--- Task 1/Task 1.2/Source.cpp
+++ Task 1/Task 1.2/Source.cpp
@@ -1,19 +1,53 @@
-#define _USE_MATH_DEFINES // for C++
+﻿#define _USE_MATH_DEFINES // for C++
 #include <cmath>
-#include <expection>
 #include <iostream>
 using namespace std;
 
 /**
-* \brief 
-* \return 
+* \brief Расчет area по заданной формуле
+* \param length Первый аргумент функции
+* \param width Второй аргумент функции
+* \return значение аrea
 */
-double get_
+double get_area(const double length, const double width);
 
+/**
+* \brief Расчет triangle_area по заданной формуле
+* \param length Первый аргумент функции
+* \param width Второй аргумент функции
+* \return значение triangle_area
+*/
+double get_triangle_area(const double length, const double width);
+
+/**
+* \brief Вхождение в программу
+* \return Возвращает 0 в случае успеха
+*/
 
 int main()
 {
-	double area = get_area();
-	
+	setlocale(LC_ALL, "Rus");
+	double length = 0.0;
+	double width = 0.0;
+	cout << "Введите длину: " << endl;
+	cin >> length;
+	cout << "Введите ширину: " << endl;
+	cin >> width;
+	const double area = get_area(length, width);
+	const double triangle_area = get_triangle_area(length, width);
+	cout << "Площадь прямоугольника равна: " << '\n' << area << '\n' << "Площадь треугольника равна: " << '\n' << triangle_area << endl;
 
+	return 0;
+}
+
+double get_area(const double length, const double width) 
+{
+	double area = length * width;
+	return area;
+}
+
+double get_triangle_area(const double length, const double width)
+{
+	double triangle_area = length * width / 2;
+	return triangle_area;
 }
