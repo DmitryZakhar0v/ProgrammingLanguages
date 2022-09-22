e46532d46c69a3257453217d059b1f30e4eb8354
DmitryZakhar0v <pcixtimessquare@gmail.com>
2022-09-22 13:57:38 +0300

    Исправленное 1.1

---
+++ Task 1/Task 1.2/Source.cpp
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include <expection>
+#include <iostream>
+using namespace std;
+
+/**
+* \brief 
+* \return 
+*/
+double get_
+
+
+int main()
+{
+	double area = get_area();
+	
+
+}
